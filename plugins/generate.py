99863dfa966e5789f54fc0bbeae639e9e2d5eea6
minedeploy <174704366+minedeploy@users.noreply.github.com>
2024-10-02 21:12:18 +0700

    Update generate.py

--- plugins/generate.py
+++ plugins/generate.py
@@ -3,7 +3,7 @@ from typing import TYPE_CHECKING
 from hydrogram import Client, filters
 from hydrogram.helpers import ikb
 
-from bot import config, filter_authorized, logger, url_safe
+from bot import cache, config, filter_authorized, logger, url_safe
 from plugins import list_available_commands
 
 if TYPE_CHECKING:
@@ -16,6 +16,9 @@ if TYPE_CHECKING:
     filter_authorized & (~filters.command(list_available_commands) & ~filters.service)
 )
 async def generate_handler(client: "bot", message: "Message") -> None:
+    if not cache.generate_status:
+        return
+
     try:
         # Copy the message to the database chat
         database_chat_id = config.DATABASE_CHAT_ID
